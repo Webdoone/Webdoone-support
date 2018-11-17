b881fa69410bbb76493a49ea23610dd6b5fc1a84
Adam <adam@MacBook-Pro.local>
2018-11-17 21:47:44 +0100

    Initial

---
+++ includes/class-webdoone-support-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://webdoone.com
+ * @since      1.0.0
+ *
+ * @package    Webdoone_Support
+ * @subpackage Webdoone_Support/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Webdoone_Support
+ * @subpackage Webdoone_Support/includes
+ * @author     Webdoone <dev@webdoone.com>
+ */
+class Webdoone_Support_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-webdoone-support-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://webdoone.com
+ * @since      1.0.0
+ *
+ * @package    Webdoone_Support
+ * @subpackage Webdoone_Support/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Webdoone_Support
+ * @subpackage Webdoone_Support/includes
+ * @author     Webdoone <dev@webdoone.com>
+ */
+class Webdoone_Support_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
